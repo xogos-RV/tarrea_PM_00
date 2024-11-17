9b84d83c57a960608b1d6f38e8066d915b33bb45
Diego Santiago <diego.santiago.cocodin@gmail.com>
2024-11-17 17:26:59 +0100

    add force

--- Assets/CONTROL/move.cs
+++ Assets/CONTROL/move.cs
@@ -3,49 +3,46 @@ using UnityEngine.InputSystem;
 
 public class ObjectMover : MonoBehaviour
 {
-    public InputActionAsset inputActions;
+    [SerializeField] InputActionAsset inputAction;
+    [SerializeField] float force;
     private InputAction moveAction;
-    public float speed = 10.0f;
+    Rigidbody rb;
+
+    void Start()
+    {
+        force = 3.0f;
+        rb = GetComponent<Rigidbody>();
+    }
 
     private void OnEnable()
     {
-        // Asignar la acción de movimiento
-        moveAction = inputActions.FindActionMap("ActionMap").FindAction("move");
+        moveAction = inputAction.FindActionMap("ActionMap").FindAction("move");
         if (moveAction != null)
         {
             moveAction.Enable();
-            //Debug.Log("Move action enabled.");
         }
         else
         {
-           // Debug.LogError("Move action not found!");
+            Debug.LogError("Move action not found!");
         }
     }
 
     private void OnDisable()
     {
         moveAction.Disable();
-       // Debug.Log("Move action disabled.");
     }
 
     void Update()
     {
         if (moveAction != null)
         {
-            // Leer el valor del joystick como un vector 2D
             Vector2 inputVector = moveAction.ReadValue<Vector2>();
-           // Debug.Log($"Input Vector: {inputVector}");
-
-            // Crear un Vector3 para el movimiento a partir del Vector2
-            Vector3 movement = new Vector3(inputVector.x, 0, inputVector.y);
-
-            // Aplicar el movimiento al objeto
-            transform.Translate(movement * speed * Time.deltaTime, Space.World);
-            //Debug.Log($"Object moved to position: {transform.position}");
+            Vector3 vectorForce = rb.GetAccumulatedForce();
+            rb.AddForce(inputVector.x + vectorForce.x * force, 0, inputVector.y + vectorForce.z * force);
         }
         else
         {
-           // Debug.LogError("Move action is not initialized.");
+            Debug.LogError("Move action is not initialized.");
         }
     }
 }
