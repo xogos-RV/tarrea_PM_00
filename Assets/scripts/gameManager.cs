d741eafa8a1b3fbab20746bd0ba15830e7872517
Diego Santiago <diego.santiago.cocodin@gmail.com>
2024-11-17 18:29:41 +0100

    resetCarPosition

---
+++ Assets/scripts/gameManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class agmeManager : MonoBehaviour
+{
+
+    Vector3 resetCarPosition;
+    [SerializeField] GameObject car;
+    // Start is called before the first frame update
+    void Start()
+    {
+        resetCarPosition = car.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            car.transform.position = resetCarPosition;
+        }
+
+        if (car.transform.position.y <= -10)
+        {
+            car.transform.position = resetCarPosition;
+        }
+    }
+}
